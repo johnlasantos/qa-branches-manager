fc0bd1693d543ef2d9b8a467ce6873eccee9a7ae
John L. Santos <johnlasantos@gmail.com>
2025-04-17 00:47:54 -0300

    Fixing the title
    Signed-off-by: John L. Santos <johnlasantos@gmail.com>

--- src/components/GitHeader.tsx
+++ src/components/GitHeader.tsx
@@ -12,7 +12,7 @@ const GitHeader: React.FC<GitHeaderProps> = ({ className }) => {
     <div className={cn("flex flex-col items-center justify-center py-4", className)}>
       <div className="flex items-center space-x-2">
         <GitBranch className="h-6 w-6 text-primary" />
-        <h1 className="text-2xl font-bold text-gray-800">scriptcase</h1>
+        <h1 className="text-2xl font-bold text-gray-800">Branches Manager</h1>
       </div>
       <a 
         href="http://athena.scriptcase.net:8092/scriptcase-git/" 
