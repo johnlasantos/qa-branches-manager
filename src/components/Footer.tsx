5a60332f3c376f83ffaa87a41bc69b11795061ce
John L. Santos <johnlasantos@gmail.com>
2025-04-27 02:31:56 -0300

    Fixing the footer.
    Signed-off-by: John L. Santos <johnlasantos@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
   
   return (
-    <footer className="mt-6 py-4 px-6 text-sm text-gray-600 text-center">
+    <footer className="py-4 px-6 text-sm text-gray-600 text-center">
       <div>Copyright © {currentYear} Scriptcase</div>
       <div>All rights reserved to QA Scriptcase</div>
     </footer>
